11678d04a81fda934eb8705648e45db91291e999
Ahmed Choulak <141917484+AhmedChlk@users.noreply.github.com>
2024-02-17 18:03:36 +0100

    Add files via upload

---
+++ Exo_Gestion_Etudiant/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Main{
+    public class Livre{
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+         public int AnneePublication { get; set; }
+         public bool LivreEmprunter { get; set; }
+        public Emprunter InfoEmprunter { get; set; }
+        public Livre (string titre, string auteur, int anneepub, bool livreEmprunter, Emprunter infoEmprunter)
+        {
+        Titre = titre;
+        Auteur = auteur;
+        AnneePublication = anneepub;
+        LivreEmprunter = livreEmprunter;
+        InfoEmprunter = infoEmprunter;
+        } 
+    }
+    public class Bibliotheque{
+        public List<Livre> ListeLivres { get; } = new List<Livre>();
+        public void AjouterLivre(Livre livre){
+            ListeLivres.Add(livre);
+        }
+        public void afficherLivre (){
+            Console.WriteLine("Voici les livres dispo dans la biblio : ");
+            foreach (var livre in ListeLivres)
+            {
+                Console.WriteLine($"{livre.Titre} - {livre.Auteur} - {livre.AnneePublication}");
+            }
+        }
+        public void EmprunterLivre (string NomLivre, Emprunter Client)
+        {
+            foreach (var livre in ListeLivres)
+            {
+                if (string.Equals(NomLivre,livre.Titre)&& !livre.LivreEmprunter)
+                {
+                    livre.InfoEmprunter = Client;
+                    livre.LivreEmprunter = true;
+                }
+            }
+        }
+    }
+    public struct Emprunter {
+        public string Nom ;
+        public string Prenom;
+        public int DateEmprunt;
+
+        public Emprunter (string nom, string prenom, int dateEmprunt)
+        {
+        Nom = nom;
+        Prenom = prenom;
+        DateEmprunt = dateEmprunt;
+        }
+    }
+
+    
+
+    class Program{
+        static void Main(string[] args)
+        {
+                    Bibliotheque maBiblio = new Bibliotheque();
+
+        Livre livre1 = new Livre("Titre1", "Auteur1", 2000, false, new Emprunter());
+        Livre livre2 = new Livre("Titre2", "Auteur2", 2010, false, new Emprunter());
+
+        maBiblio.AjouterLivre(livre1);
+        maBiblio.AjouterLivre(livre2);
+
+        maBiblio.afficherLivre();
+
+        Emprunter client = new Emprunter("ClientNom", "ClientPrenom", 20220105);
+        maBiblio.EmprunterLivre("Titre1", client);
+
+        maBiblio.afficherLivre();
+        }
+    }
+    
+}
---
+++ Exo_Gestionnaire_Contacts/GestionnaireContacts/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace GestionDeContacts
+{
+    class Contact
+    {
+        private string nom;
+        private string prenom;
+        private string adresse;
+
+        public string Nom
+        {
+            get { return nom; }
+            set { if (!string.IsNullOrEmpty(value)) nom = value; }
+        }
+        public string Prenom
+        {
+            get { return prenom; }
+            set { if (!string.IsNullOrEmpty(value)) prenom = value; }
+        }
+        public string Adresse
+        {
+            get { return adresse; }
+            set { if (!string.IsNullOrEmpty(value)) adresse = value; }
+        }
+        public Contact(string nom, string prenom, string adresse)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Adresse = adresse;
+        }
+    }
+    class ListeContacts
+    {
+        private List<Contact> contacts = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+        public void SuppContact(string nom)
+{
+    bool find = false;
+    for (int i = contacts.Count - 1; i >= 0; i--)
+    {
+        var contact = contacts[i];
+        if (String.Equals(nom, contact.Nom))
+        {
+            contacts.Remove(contact);
+            Console.WriteLine("Contact supprimé");
+            find = true;
+        }
+    }
+    if (!find)
+    {
+        Console.WriteLine("Contact inexistant");
+    }
+}
+
+
+
+public void RechercherContact(string search)
+{
+    bool find = false;
+    foreach (var contact in contacts)
+    {
+        if (String.Equals(search, contact.Nom) || String.Equals(search, contact.Prenom) || String.Equals(search, contact.Adresse))
+        {
+            Console.WriteLine($"Contact Trouvé Nom : {contact.Nom} prenom : {contact.Prenom} adresse : {contact.Adresse}");
+            find = true;
+        }
+    }
+    if (!find)
+    {
+        Console.WriteLine("Conctact inexistant");
+    }
+
+}
+        public void AfficherContact ()
+        {
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"Nom: {contact.Nom}, Prénom: {contact.Prenom}, Adresse: {contact.Adresse}");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listeContacts = new ListeContacts();
+
+            var contact1 = new Contact("Doe", "John", "123 Main St");
+            var contact2 = new Contact("Smith", "Jane", "456 Elm St");
+
+            listeContacts.AddContact(contact1);
+            listeContacts.AddContact(contact2);
+
+            listeContacts.RechercherContact("Smith");
+
+             listeContacts.AfficherContact();
+            listeContacts.SuppContact("Smith");
+            listeContacts.AfficherContact();
+        }
+    }
+}
